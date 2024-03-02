29c4cc9607b1c4e7af206b5c4620a7607be64d11
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2024-03-02 23:36:30 +0530

    Added - First Grids

---
+++ components/Grid.tsx
@@ -0,0 +1,81 @@
+"use client";
+const Grid = () => {
+
+  // useEffec
+
+  return (
+    <div className="h-screen">
+      <div className="row h-2/6">
+        <div className="col-4">
+          <div className="card shadow-lg h-100 relative" id="new-card-1">
+            <div className="absolute top-0 right-0 p-[10px] z-[2]"></div>
+
+            <div id="new-chart-1" className="h-100 m-[2%]"></div>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card shadow-lg h-100 relative" id="new-card-2">
+            <div className="absolute top-0 right-0 p-[10px] z-[2]"></div>
+
+            <div id="new-chart-2" className="h-100 m-[2%]"></div>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card shadow-lg h-100 relative" id="new-card-3">
+            <div className="absolute top-0 right-0 p-[10px] z-[2]"></div>
+
+            <div id="new-chart-3" className="h-100 m-[2%]"></div>
+          </div>
+        </div>
+      </div>
+      <div className="row h-2/6 mt-1">
+        <div className="col-4">
+          <div className="card shadow-lg h-100 relative" id="new-card-4">
+            <div className="absolute top-0 right-0 p-[10px] z-[2]"></div>
+
+            <div id="new-chart-4" className="h-100 m-[2%]"></div>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card shadow-lg h-100 relative" id="new-card-5">
+            <div className="absolute top-0 right-0 p-[10px] z-[2]"></div>
+
+            <div id="new-chart-5" className="h-100 m-[2%]"></div>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card shadow-lg h-100 relative" id="new-card-6">
+            <div className="absolute top-0 right-0 p-[10px] z-[2]"></div>
+
+            <div id="new-chart-6" className="h-100 m-[2%]"></div>
+          </div>
+        </div>
+      </div>
+      <div className="row h-2/6 mt-1">
+        <div className="col-4">
+          <div className="card shadow-lg h-100 relative" id="new-card-7">
+            <div className="absolute top-0 right-0 p-[10px] z-[2]"></div>
+
+            <div id="new-chart-7" className="h-100 m-[2%]"></div>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card shadow-lg h-100 relative" id="new-card-8">
+            <div className="absolute top-0 right-0 p-[10px] z-[2]"></div>
+
+            <div id="new-chart-8" className="h-100 m-[2%]"></div>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card shadow-lg h-100 relative" id="new-card-9">
+            <div className="absolute top-0 right-0 p-[10px] z-[2]"></div>
+
+            <div id="new-chart-9" className="h-100 m-[2%]"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Grid;
