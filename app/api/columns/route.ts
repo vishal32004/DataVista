2c669f07a0cdcd6269c93f4415d873cec3d3b0f5
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2024-03-17 14:23:46 +0530

    Modified - Routes

--- app/api/columns/route.ts
+++ app/api/columns/route.ts
@@ -3,13 +3,19 @@ import { NextResponse } from "next/server";
 
 export const GET = async (req: Request, res: NextResponse) => {
     try {
-
         const { searchParams } = new URL(req.url);
         const selectedPage = searchParams.get("selectedPage");
-        const result = await pool.query(`SELECT ColumnName FROM ColumnNames WHERE Pagename = '${selectedPage}'`);
-        console.log(`SELECT ColumnName FROM ColumnNames WHERE Pagename = '${selectedPage}'`)
+
+        if (!selectedPage) {
+            return new NextResponse("Invalid parameters", { status: 400 });
+        }
+
+        const result = await pool.query(
+            `SELECT ColumnName FROM ColumnNames WHERE Pagename = $1`,
+            [selectedPage]
+        );
+
         const data = result.rows;
-        console.log(data)
         return NextResponse.json(data);
     } catch (error) {
         console.error("Error executing SQL query:", error);
