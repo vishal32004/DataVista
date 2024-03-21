18a6c39ce09bc653eabeb30c87b92923cf92b9b3
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2024-03-21 21:34:34 +0530

    MOdified

---
+++ app/api/getPrefix/route.ts
@@ -0,0 +1,24 @@
+import { pool } from "@/lib/db-config";
+import { NextResponse } from "next/server";
+
+export const GET = async (req: Request, res: NextResponse) => {
+    try {
+        const { searchParams } = new URL(req.url);
+        const selectedPage = searchParams.get("selectedPage");
+
+        if (!selectedPage) {
+            return new NextResponse("Invalid parameters", { status: 400 });
+        }
+
+        const result = await pool.query(
+            `SELECT prefix FROM prefix WHERE Pagename = $1`,
+            [selectedPage]
+        );
+
+        const data = result.rows;
+        return NextResponse.json(data);
+    } catch (error) {
+        console.error("Error executing SQL query:", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
